eeb8152ccd1e44652d8408ed03475a42158f6887
dhillon64 <67638060+dhillon64@users.noreply.github.com>
2020-11-09 15:44:44 +0000

    Add project files.

---
+++ ParkiAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkiAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {
+          
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; } 
+    }
+}
