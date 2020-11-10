a3ff7116ae8ea93947d1868d4e270b5b598eba69
dhillon64 <67638060+dhillon64@users.noreply.github.com>
2020-11-10 02:24:44 +0000

    Added National parks controller along with http methods GET and POST.

--- ParkiAPI/Models/Dtos/NationalParkDto.cs
+++ ParkiAPI/Models/Dtos/NationalParkDto.cs
@@ -11,10 +11,10 @@ namespace ParkiAPI.Models.Dtos
         
         public int Id { get; set; }
 
-        
+        [Required]
         public string Name { get; set; }
         
-        
+        [Required]
         public string State { get; set; }
 
         public DateTime Created { get; set; }
